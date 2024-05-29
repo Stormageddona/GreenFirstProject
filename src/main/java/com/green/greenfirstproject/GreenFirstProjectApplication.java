96401586dccbc527aa87d7ca05ce9bb7264f6959
GYB <gsb0708@gmail.com>
2024-05-29 14:41:46 +0900

    first project setting

---
+++ src/main/java/com/green/greenfirstproject/GreenFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.green.greenfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenFirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenFirstProjectApplication.class, args);
+    }
+
+}
