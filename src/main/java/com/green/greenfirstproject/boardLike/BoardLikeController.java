a1f35ef92312d43e692539446c055b5e74490dcb
Administrator <dlwnsguq2002@gmail.com>
2024-06-12 15:35:50 +0900

    수정 필요할수있지만 1차 완성본

--- src/main/java/com/green/greenfirstproject/boardLike/BoardLikeController.java
+++ src/main/java/com/green/greenfirstproject/boardLike/BoardLikeController.java
@@ -1,15 +1,36 @@
 package com.green.greenfirstproject.boardLike;
 
+import com.green.greenfirstproject.boardLike.model.FeedLike;
+import com.green.greenfirstproject.common.ResultDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.green.greenfirstproject.common.GlobalConst.ERROR_CODE;
+import static com.green.greenfirstproject.common.GlobalConst.SUCCESS_CODE;
 
 
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping
+@RequestMapping("api/community/like")
 @RestController
 public class BoardLikeController {
+    private final BoardLikeService service;
+
+    @GetMapping
+    @Operation(summary = "피드 좋아요 ")
+    public ResultDto<Integer> postCommunityFavorite(@ParameterObject @ModelAttribute FeedLike p){
+
+        try {
+            int res = service.postCommunityFavorite(p);
+            return ResultDto.resultDto(SUCCESS_CODE,res == 0 ? "좋아요 취소" : "좋아요 성공",res);
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResultDto.resultDto1(ERROR_CODE,"좋아요 실패");
+        }
+    }
 }
