1b2a79dbc814f25c760fb3cfffd5e62677eeac51
Administrator <dlwnsguq2002@gmail.com>
2024-06-12 14:28:51 +0900

    수정 필요할수있지만 1차 완성본

---
+++ src/main/java/com/green/greenfirstproject/boardLike/BoardLikeController.java
@@ -0,0 +1,15 @@
+package com.green.greenfirstproject.boardLike;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping
+@RestController
+public class BoardLikeController {
+}
--- src/main/java/com/green/greenfirstproject/boardLike/BoardLikeService.java
+++ src/main/java/com/green/greenfirstproject/boardLike/BoardLikeService.java
@@ -1,5 +1,13 @@
 package com.green.greenfirstproject.boardLike;
 
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class BoardLikeService {
 
 
